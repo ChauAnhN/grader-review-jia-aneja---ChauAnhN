34673543ae50d8efca712e41a9d32a0d9f64bd32
ChauAnhN <130714987+ChauAnhN@users.noreply.github.com>
2024-03-07 12:38:43 -0800

    commit successful

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 rm -rf grading-area
@@ -8,8 +8,15 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [ -f "student-submission/ListExamples.java" ]; then
+  echo "file found"
+else
+  echo "file ListExamples.java not found"
+  exit 1
+fi 
 
-# Draw a picture/take notes on the directory structure that's set up after
+cp student-submission/ListExamples.java grading-area/
+cp TestListExamples.java grading-area/git push origin main
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
